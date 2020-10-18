7474b289637eb53e049022aa3dedb932ec9132e7
Kelin Field <kfield111@hotmail.com>
2020-10-18 02:33:35 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,8 +24,6 @@ def can_be_created_with_a_hash_of_attributes
 end
 
 def can_be_created_in_a_block(title = "Home Alone", release_date = "1990")
- movie = Movie.create(attributes)
-
   Movie.create do |m|
     m.tite = "test"
   end
