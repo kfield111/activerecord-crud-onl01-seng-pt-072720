abd32259736d893bf258971441bb984e4c1f6432
Kelin Field <kfield111@hotmail.com>
2020-10-18 02:11:22 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -12,7 +12,7 @@ def can_be_instantiated_and_then_saved
 end
 
 def can_be_created_with_a_hash_of_attributes
-  # Initialize movie and then and save it
+  movie = Movie.create(attributes)
   attributes = {
       title: "The Sting",
       release_date: 1973,
