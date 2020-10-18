b9ffe4b5cd611ab47f9d12e6e3a45d14e62bb83d
Kelin Field <kfield111@hotmail.com>
2020-10-18 03:08:55 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -25,7 +25,7 @@ end
 
 def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   movie = Movie.create do |m|
-    m.title = title
+    m.title = "The Room"
     m.release_date = 2003
   end
 end
