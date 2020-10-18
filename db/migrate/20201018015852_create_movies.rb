c5c877b5b5114a87a52c6db96566344823edfecb
Kelin Field <kfield111@hotmail.com>
2020-10-18 02:03:46 +0000

    Automatically backed up by Learn

--- db/migrate/20201018015852_create_movies.rb
+++ db/migrate/20201018015852_create_movies.rb
@@ -1,6 +1,6 @@
 class CreateMovies < ActiveRecord::Migration[5.2]
   def change
-    add_columns :movies do |t|
+    create_table :movies do |t|
       t.string :tite
       t.integer :release_date
       t.sring :director
