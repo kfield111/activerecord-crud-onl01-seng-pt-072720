acfbe33b258c576ee9627b36830b3151129e0c7c
Kelin Field <kfield111@hotmail.com>
2020-10-18 02:35:21 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,9 +23,9 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block
+def can_be_created_in_a_block(title = "test")
   Movie.create do |m|
-    m.tite = "test"
+    m.tite = "Jump"
   end
 end
 
