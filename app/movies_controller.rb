72a362786fd565f698ca0d46b998ea8decc1d90c
Kelin Field <kfield111@hotmail.com>
2020-10-18 02:34:41 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -23,7 +23,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(title = "Home Alone")
+def can_be_created_in_a_block
   Movie.create do |m|
     m.tite = "test"
   end
